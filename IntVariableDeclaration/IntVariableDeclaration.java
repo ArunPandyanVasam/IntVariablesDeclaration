6318bd31de05d2e4defd95f83334d1974f77eb5f
ArunPandyanVasam <you@example.com>
2023-06-03 18:25:36 -0400

    version1

---
+++ IntVariableDeclaration/IntVariableDeclaration.java
@@ -0,0 +1,13 @@
+
+
+public class IntVariableDeclaration {
+    public static void main(String[] args) {
+        int x;
+        x = 106;
+        System.out.println(x);
+        
+        int y = 145;
+         System.out.println(y);
+        
+    }
+}
